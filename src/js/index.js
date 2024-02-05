be7ef2348445956ad21bdb184006ae792b2bb3b1
Alvin Al <alvvxal@gmail.com>
2024-02-05 22:23:52 +0700

    Fix button for hover and click.

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,28 @@ openHamburgerBar = () => {
 };
 
 
-change = (src) => {
+changePicture = (src) => {
     const mainPicture = document.getElementById('main-picture');
     const thumbPicture = document.getElementById('src')
 
     mainPicture.src = src;
-    thumbPicture.classList.replace('opacity-60','opacity-100');
+    thumbPicture.classList.add('focus');
+};
+
+
+activeButton = (clickedButton) => {
+  let buttons = ['white-button', 'black-button'];
+
+  buttons.forEach(element => {
+    let currentButton = document.getElementById(element);
+
+    if(currentButton === clickedButton){
+        currentButton.style.backgroundColor = 'bg-gray-800';
+        currentButton.style.color = '#f6f6f6';
+    }else{
+        currentButton.style.backgroundColor = '';
+        currentButton.style.color = '';
+    }
+  });
+
 };
