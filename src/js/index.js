47ec624e976d54588ed1f99631bef7d864bef169
Alvin Al <alvvxal@gmail.com>
2024-02-09 07:06:52 +0700

    Add interaction for main picture and the thumbnail when clicked

--- src/js/index.js
+++ src/js/index.js
@@ -9,39 +9,19 @@ openHamburgerBar = () => {
 };
 
 
+
+
+
 changePicture = (src, id) => {
-    const mainPicture = document.getElementById('main-picture');
-    const thumbPicture = document.getElementById('src');
-    const firstPic = document.getElementById('pic-1');
-    const choPic = document.getElementById('id');
+    const currentImage = document.getElementById('main-picture');
+    const newImage = document.getElementById(id);
+    const allThumb = document.getElementsByClassName('img');
 
-    mainPicture.src = src;
-    
-
-    if(thumbPicture !== firstPic){
-        firstPic.classList.replace('opacity-100', 'opacity-60');
-    };
-
-    choPic.classList.add('focus:opacity-100');
-    
-
-
-};
-
-
-activeButton = (clickedButton) => {
-  let buttons = ['white-button', 'black-button'];
-
-  buttons.forEach(element => {
-    let currentButton = document.getElementById(element);
-
-    if(currentButton === clickedButton){
-        currentButton.style.backgroundColor = 'bg-gray-800';
-        currentButton.style.color = '#f6f6f6';
-    }else{
-        currentButton.style.backgroundColor = '';
-        currentButton.style.color = '';
+    for(let i = 0; i < allThumb.length; i++){
+        allThumb[i].classList.replace('opacity-100', 'opacity-60');
     }
-  });
+
+    newImage.classList.replace('opacity-60', 'opacity-100');
+    currentImage.src = src;
 
 };
